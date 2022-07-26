e3c2af81b7baa89b1900e9a66d83df3cf487ee30
Mary Nika <elanium@gmail.com>
2022-07-26 20:47:15 +0300

    Comments_allerts_variables

--- scripts/JS_first_example.js
+++ scripts/JS_first_example.js
@@ -1,4 +1,11 @@
+/* this will be adde to the console
+just to learn it*/
 console.log('test');
 console.log('---------------');
 console.log('work with Visual Studio Code');
-document.write("<h1>Hello world!!!</h1>");
+var taxtToHeader = "<h1>Hello world!!!</h1>"
+// This is a variable to use it in the header
+document.write(taxtToHeader);
+
+//modal message
+alert ("Are you shure?");
